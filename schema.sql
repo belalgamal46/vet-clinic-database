0ba4fb4807e8120a8868374334bac4b5976e3fe3
belalgamal46 <belalgamal00@gmail.com>
2023-01-21 01:08:04 +0200

    Add new table schemas

--- schema.sql
+++ schema.sql
@@ -7,6 +7,21 @@ CREATE TABLE animals (
   escape_attempts INT,
   neutered boolean,
   weight_kg decimal,
-  species VARCHAR(250),
+  species_id INT REFERENCES species(id),
+  owner_id INT REFERENCES owners(id),
   PRIMARY KEY(id)
   );
+
+  CREATE TABLE owners(
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(250),
+  age INT,
+  PRIMARY KEY (id)
+  );
+
+  CREATE TABLE species(
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(250),
+  PRIMARY KEY (id)
+  );
+
