fd938ccbcb84f983e761d90d4133add488cc094e
belalgamal46 <belalgamal00@gmail.com>
2023-01-21 07:54:23 +0200

    Insert vets, specializations, and visits data

--- data.sql
+++ data.sql
@@ -1,21 +1,28 @@
 /* Populate database with sample data. */
+INSERT INTO
+  animals (
+    name,
+    date_of_birth,
+    weight_kg,
+    neutered,
+    escape_attempts
+  )
+VALUES
+  ('Agumon', '2020-02-03', 10.23, true, 0),
+  ('Gabumon', '2018-11-15', 8, true, 2),
+  ('Pikachu', '2021-01-07', 15.04, false, 1),
+  ('Devimon', '2017-05-12', 11, true, 5),
+  ('Charmander', '2020-02-08', -11, false, 0),
+  ('Plantmon', '2021-11-15', -5.7, true, 2),
+  ('Squirtle', '1993-04-02', -12.13, true, 3),
+  ('Angemon', '2005-01-12', -45, true, 1),
+  ('Boarmon', '2005-01-07', 20.4, true, 7),
+  ('Blossom', '1998-10-13', 17, true, 3),
+  ('Ditto', '2022-05-14', 22, true, 4);
 
-INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts) 
-  VALUES 
-  ('Agumon', '2020-02-03',10.23, true, 0),
-  ('Gabumon','2018-11-15', 8, true, 2),
-  ('Pikachu', '2021-01-07',15.04,false, 1),
-  ('Devimon', '2017-05-12',11,true, 5),
-  ('Charmander', '2020-02-08',-11,false,0),
-  ('Plantmon','2021-11-15',-5.7,true,2),
-  ('Squirtle','1993-04-02',-12.13,true,3),
-  ('Angemon','2005-01-12',-45,true,1),
-  ('Boarmon','2005-01-07',20.4,true,7),
-  ('Blossom','1998-10-13',17,true,3),
-  ('Ditto','2022-05-14',22,true,4);
-
-  INSERT INTO owners (full_name, age)
-  VALUES
+INSERT INTO
+  owners (full_name, age)
+VALUES
   ('Sam Smith', 34),
   ('Jennifer Orwell', 19),
   ('Bob', 45),
@@ -23,7 +30,55 @@ INSERT INTO animals (name,date_of_birth,weight_kg,neutered,escape_attempts)
   ('Dean Winchester', 14),
   ('Jodie Whittaker', 38);
 
-  INSERT INTO species (name)
-  VALUES
+INSERT INTO
+  species (name)
+VALUES
   ('Pokemon'),
   ('Digimon');
+
+INSERT INTO
+  vets (name, age, date_of_graduation)
+VALUES
+  ('William Tatcher', 45, '2000-04-23'),
+  ('Maisy Smith', 26, '2019-01-17'),
+  ('Stephanie Mendez', 64, '1981-05-04'),
+  ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO
+  vets (name, age, date_of_graduation)
+VALUES
+  ('William Tatcher', 45, '2000-04-23'),
+  ('Maisy Smith', 26, '2019-01-17'),
+  ('Stephanie Mendez', 64, '1981-05-04'),
+  ('Jack Harkness', 38, '2008-06-08');
+
+INSERT INTO
+  specializations (species_id, vets_id)
+VALUES
+  (1, 1),
+  (1, 3),
+  (2, 3),
+  (2, 4);
+
+INSERT INTO
+  visits (animals_id, vets_id, visit_date)
+VALUES
+  (1, 1, '2020-05-24'),
+  (1, 3, '2020-07-22'),
+  (2, 4, '2021-02-02'),
+  (3, 2, '2020-01-05'),
+  (3, 2, '2020-03-08'),
+  (3, 2, '2020-05-14'),
+  (4, 3, '2021-05-04'),
+  (5, 4, '2021-02-24'),
+  (6, 2, '2019-12-21'),
+  (6, 1, '2020-08-10'),
+  (6, 2, '2021-04-07'),
+  (7, 3, '2019-09-29'),
+  (8, 4, '2020-10-03'),
+  (8, 4, '2020-11-04'),
+  (9, 2, '2019-01-24'),
+  (9, 2, '2019-05-15'),
+  (9, 2, '2020-02-27'),
+  (9, 2, '2020-08-03'),
+  (10, 3, '2021-01-11');
