889d0c3483a48ed3b81eef5080ab4ddcf2964a13
belalgamal46 <belalgamal00@gmail.com>
2023-01-24 15:25:16 +0200

    Add many to many relationship

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -1,45 +1,59 @@
 CREATE TABLE
-  patients (
-    id SERIAL,
-    name VARCHAR(250),
-    date_of_birth date,
-    PRIMARY KEY (id)
+  "patients" (
+    "id" SERIAL PRIMARY KEY,
+    "name" VARCHAR(250),
+    "date_of_birth" date
   );
 
 CREATE TABLE
-  treatments (
-    id INT REFERENCES medical_histories (id),
-    type VARCHAR(250),
-    name VARCHAR(250),
-    PRIMARY KEY (id)
+  "treatments" (
+    "id" SERIAL PRIMARY KEY,
+    "type" VARCHAR(250),
+    "name" VARCHAR(250)
   );
 
 CREATE TABLE
-  invoices (
-    id SERIAL,
-    total_amount decimal(2, 20),
-    generated_at timestamp,
-    payed_at timestamp,
-    medical_history_id INT REFERENCES medical_histories (id),
-    PRIMARY KEY (id)
+  "invoices" (
+    "id" SERIAL PRIMARY KEY,
+    "total_amount" "decimal(2, 20)",
+    "generated_at" timestamp,
+    "payed_at" timestamp,
+    "medical_history_id" INT
   );
 
 CREATE TABLE
-  medical_histories (
-    id SERIAL,
-    admitted_at timestamp,
-    patience_id INT REFERENCES patients (id),
-    status VARCHAR(250),
-    PRIMARY KEY (id)
+  "medical_histories" (
+    "id" SERIAL PRIMARY KEY,
+    "admitted_at" timestamp,
+    "patience_id" INT,
+    "status" VARCHAR(250)
   );
 
 CREATE TABLE
-  invoices_items (
-    id SERIAL,
-    unit_price decimal(2, 20),
-    quantity INT,
-    total_price decimal(2, 20),
-    invoice_id INT REFERENCES invoices (id),
-    treatments_id INT REFERENCES treatments (id),
-    PRIMARY KEY (id)
+  "invoices_items" (
+    "id" SERIAL PRIMARY KEY,
+    "unit_price" "decimal(2, 20)",
+    "quantity" INT,
+    "total_price" "decimal(2, 20)",
+    "invoice_id" INT,
+    "treatments_id" INT
   );
+
+CREATE TABLE
+  "treatments_and_medical_histories" (
+    "medical_histories_id" INT,
+    "treatments_id" INT,
+    PRIMARY KEY ("medical_histories_id", "treatments_id")
+  );
+
+ALTER TABLE "invoices" ADD FOREIGN KEY ("medical_history_id") REFERENCES "medical_histories" ("id");
+
+ALTER TABLE "medical_histories" ADD FOREIGN KEY ("patience_id") REFERENCES "patients" ("id");
+
+ALTER TABLE "invoices_items" ADD FOREIGN KEY ("invoice_id") REFERENCES "invoices" ("id");
+
+ALTER TABLE "invoices_items" ADD FOREIGN KEY ("treatments_id") REFERENCES "treatments" ("id");
+
+ALTER TABLE "treatments_and_medical_histories" ADD FOREIGN KEY ("medical_histories_id") REFERENCES "medical_histories" ("id");
+
+ALTER TABLE "treatments_and_medical_histories" ADD FOREIGN KEY ("treatments_id") REFERENCES "treatments" ("id");
